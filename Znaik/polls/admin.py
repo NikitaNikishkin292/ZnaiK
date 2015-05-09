5581855ed2f5ed31a5bdc727bf15ea38ec914c56
NikitaNikishkin292 <nikita.nikishkin@gmail.com>
2015-05-09 21:19:17 +0300

    admin

--- Znaik/polls/admin.py
+++ Znaik/polls/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from .models import Choise, Question
+
 # Register your models here.
+class QuestionAdmin(admin.ModelAdmin):
+	fieldsets = [
+		(None, {'fields': ['question_text']}),
+		('Date information', {'fields': ['pub_date']}),
+	]
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choise)
